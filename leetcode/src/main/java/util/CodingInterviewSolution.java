ee80a267515b7e5d5b2a60d5f8cdd857a81794a6
ysj <a1584809881@gmail.com>
2022-12-07 00:24:01 +0800

    剑指 Offer 03. 数组中重复的数字

---
+++ leetcode/src/main/java/util/CodingInterviewSolution.java
@@ -0,0 +1,25 @@
+package util;
+
+/**
+ * 剑指offer题库
+ */
+public class CodingInterviewSolution {
+    // 剑指 Offer 03. 数组中重复的数字
+    public int findRepeatNumber(int[] nums) {
+        int i = 0, temp;
+        while (i < nums.length){
+            if (nums[i] == i){
+                i++;
+                continue;
+            }
+            if (nums[i] == nums[nums[i]]){
+                return nums[i];
+            }
+            temp = nums[i];
+            nums[i] = nums[temp];
+            nums[temp] = temp;
+        }
+        return -1;
+    }
+
+}
---
+++ leetcode/src/test/java/util/CodingInterviewSolutionTest.java
@@ -0,0 +1,21 @@
+package util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodingInterviewSolutionTest {
+
+    CodingInterviewSolution solution;
+
+    @Before
+    public void beforeClass(){
+        solution = new CodingInterviewSolution();
+    }
+
+    @Test
+    public void findRepeatNumber() {
+        assertEquals(2, solution.findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
+    }
+}
