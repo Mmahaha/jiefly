48a61633c62c202cb58ecafa898b46981c0681bd
ysj <a1584809881@gmail.com>
2022-12-07 22:42:51 +0800

    剑指 Offer 04. 二维数组中的查找

--- leetcode/src/main/java/CodingInterviewSolution.java
+++ leetcode/src/main/java/CodingInterviewSolution.java
@@ -20,4 +20,25 @@ public class CodingInterviewSolution {
         return -1;
     }
 
+    // 剑指 Offer 04. 二维数组中的查找
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int x = 0;
+        int y = matrix[0].length - 1;
+        while (x < matrix.length && y >= 0){
+            int cur = matrix[x][y];
+            if (cur == target){
+                return true;
+            }
+            if (cur > target){
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+
 }
--- leetcode/src/test/java/CodingInterviewSolutionTest.java
+++ leetcode/src/test/java/CodingInterviewSolutionTest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import util.JUtils;
 
 import static org.junit.Assert.*;
 
@@ -15,5 +16,11 @@ public class CodingInterviewSolutionTest {
     @Test
     public void findRepeatNumber() {
         assertEquals(2, solution.findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
+    }
+
+    @Test
+    public void findNumberIn2DArray() {
+        assertTrue(solution.findNumberIn2DArray(JUtils.resolveString("[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]"), 5));
+        assertFalse(solution.findNumberIn2DArray(JUtils.resolveString("[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]"), 20));
     }
 }
