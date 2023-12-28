ceceb35f779d73ee25cc5d1b57ab241bab19b889
ysj <a1584809881@gmail.com>
2023-12-28 20:16:25 +0800

    101. 对称二叉树

--- leetcode/src/main/java/solution/Hot100Solution.java
+++ leetcode/src/main/java/solution/Hot100Solution.java
@@ -1113,6 +1113,25 @@ public class Hot100Solution {
         return root;
     }
 
+    // 101. 对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return _isSymmetric(root.left, root.right);
+    }
+
+    public boolean _isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right != null) {
+            return left.val == right.val &&
+                    _isSymmetric(left.left, right.right) && _isSymmetric(left.right, right.left);
+        }
+        return false;
+    }
+
 
     private static class Node {
         Node prev;
