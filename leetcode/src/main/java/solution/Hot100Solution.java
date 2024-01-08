f8a881afefc483f2e527e3f3dd8b2320605bf6e6
ysj <a1584809881@gmail.com>
2024-01-08 20:33:28 +0800

    136. 只出现一次的数字

--- leetcode/src/main/java/solution/Hot100Solution.java
+++ leetcode/src/main/java/solution/Hot100Solution.java
@@ -1399,6 +1399,15 @@ public class Hot100Solution {
         return res;
     }
 
+    // 136. 只出现一次的数字
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
     private static class Node {
         Node prev;
         Node next;
