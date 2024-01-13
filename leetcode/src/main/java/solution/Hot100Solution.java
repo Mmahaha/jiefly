009a52ed41f7243a38018ca81ef0741fb3a11d58
ysj <a1584809881@gmail.com>
2024-01-13 23:38:40 +0800

    279. 完全平方数

--- leetcode/src/main/java/solution/Hot100Solution.java
+++ leetcode/src/main/java/solution/Hot100Solution.java
@@ -1481,6 +1481,18 @@ public class Hot100Solution {
         return dp[nums.length - 1];
     }
 
+    // 279. 完全平方数
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
     private static class Node {
         Node prev;
         Node next;
