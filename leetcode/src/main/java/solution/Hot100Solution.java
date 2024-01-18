7649ae2e0ab3ffa3296730d7cd5c1477df3d0c07
ysj <a1584809881@gmail.com>
2024-01-18 23:07:54 +0800

    152. 乘积最大子数组

--- leetcode/src/main/java/solution/Hot100Solution.java
+++ leetcode/src/main/java/solution/Hot100Solution.java
@@ -1558,6 +1558,21 @@ public class Hot100Solution {
         return len;
     }
 
+    // 152. 乘积最大子数组
+    public int maxProduct(int[] nums) {
+        int[] dpMax = new int[nums.length];
+        int[] dpMin = new int[nums.length];
+        dpMax[0] = nums[0];
+        dpMin[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dpMax[i] = Math.max(dpMin[i-1] * nums[i], Math.max(dpMax[i-1] * nums[i], nums[i]));
+            dpMin[i] = Math.min(dpMax[i-1] * nums[i], Math.min(dpMin[i-1] * nums[i], nums[i]));
+            res = Math.max(dpMax[i], res);
+        }
+        return res;
+    }
+
     private static class Node {
         Node prev;
         Node next;
