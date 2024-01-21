8d95a76c9f5da0f081ca2669ee66d240b526a76d
ysj <a1584809881@gmail.com>
2024-01-21 23:55:23 +0800

    1143. 最长公共子序列

--- leetcode/src/main/java/solution/Hot100Solution.java
+++ leetcode/src/main/java/solution/Hot100Solution.java
@@ -1593,6 +1593,26 @@ public class Hot100Solution {
         return dp[targetSum];
     }
 
+    // 1143. 最长公共子序列
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                boolean curEquals = text1.charAt(i) == text2.charAt(j);
+                if (i == 0 && j == 0) {
+                    dp[i][j] = curEquals ? 1 : 0;
+                } else if (i == 0) {
+                    dp[i][j] = curEquals ? 1 : dp[i][j-1];
+                } else if (j == 0) {
+                    dp[i][j] = curEquals ? 1 : dp[i-1][0];
+                } else {
+                    dp[i][j] = curEquals ? dp[i-1][j-1] + 1 : Math.max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+        return dp[text1.length() - 1][text2.length() - 1];
+    }
+
     private static class Node {
         Node prev;
         Node next;
